476a22ff771de4c283ccdb7c286fc41ec4e99ae9
Vemula Nishanth <165542527+Nishanth-2024@users.noreply.github.com>
2024-10-08 18:19:03 +0530

    Menubar Only App - Bolierplate

--- BreakerApp/BreakerAppApp.swift
+++ BreakerApp/BreakerAppApp.swift
@@ -9,10 +9,21 @@ import SwiftUI
 
 @main
 struct BreakerAppApp: App {
+    @State var currentNumber: String = "1"
+    
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        MenuBarExtra(currentNumber, systemImage: "\(currentNumber).circle") {
+            Button("One") { currentNumber = "1" }
+                .keyboardShortcut("1")
+            Button("Two") { currentNumber = "2" }
+                .keyboardShortcut("2")
+            Button("Three") { currentNumber = "3" }
+                .keyboardShortcut("3")
+            Divider()
+
+            Button("Quit") {
+                NSApplication.shared.terminate(nil)
+            }.keyboardShortcut("q")
         }
-        .windowResizability(.contentSize)
     }
 }
